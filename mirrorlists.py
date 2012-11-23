bcdcb438459b58a4ebf2da1890e124fca05931fa
Andrew Hamilton <ahamilton55@gmail.com>
2012-11-23 15:38:19 -0800

    Initial commit
    - Working production of mirror list files.
    - Defaults to CentOS 6 x86_64 for Eucalyptus latest (Set in config)
    - Config has a list of fake mirrors currently for ease of use.

---
+++ mirrorlists.py
@@ -0,0 +1,74 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import Response
+from ConfigParser import SafeConfigParser
+import pygeoip
+from incf.countryutils import transformations
+
+app = Flask(__name__)
+
+config = SafeConfigParser(allow_no_value=True)
+#config.read('eucalyptus_mirrors.conf')
+
+def get_continent(host_ip):
+    geoip = pygeoip.GeoIP(config.get('Config', 'GeoIPData'))
+
+    country = geoip.country_code_by_addr(host_ip)
+    if country == '':
+        return 'North America'
+    else:
+        return transformations.cca_to_ctn(country)
+
+def build_list_from_config(continent, mirrors):
+    for key,value in config.items(continent):
+        mirrors.append(key)
+
+    return mirrors
+
+def get_mirrors(continent):
+    mirrors = [] 
+
+    if continent == 'North America':
+        for cur_continent in ['North America', 'Europe', 'South America', 'Asia', 'Africa', 'Oceania', 'Antartica']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+    elif continent == 'Europe':
+        for cur_continent in ['Europe', 'North America', 'South America', 'Asia', 'Africa', 'Oceania', 'Antartica']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+    elif continent == 'Asia':
+        for cur_continent in ['Asia', 'Europe', 'North America', 'South America', 'Africa', 'Oceania', 'Antartica']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+    elif continent == 'South America':
+        for cur_continent in ['South America', 'North America', 'Europe', 'Asia', 'Africa', 'Oceania', 'Antartica']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+    elif continent == 'Africa':
+        for cur_continent in ['Africa', 'Europe', 'North America', 'South America', 'Asia', 'Oceania', 'Antartica']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+    elif continent == 'Oceania':
+        for cur_continent in ['Oceania', 'North America', 'Asia', 'Europe', 'South America', 'Africa', 'Antartica']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+    else:
+        for cur_continent in ['Antartica', 'Oceania', 'Africa', 'South America', 'Asia', 'Europe', 'North America']:
+            mirrors = build_list_from_config(cur_continent, mirrors)
+
+    return mirrors
+
+@app.route('/mirrors', methods=['POST', 'PUT', 'GET'])
+def request_mirrorlist():
+    config.read('eucalyptus_mirrors.conf')
+
+    continent = get_continent(str(request.remote_addr))
+    mirrors = get_mirrors(continent)
+
+    release = request.values.get('release', '6')
+    arch = request.values.get('arch', 'x86_64')
+    distro = request.values.get('distro', 'centos')
+    version = request.values.get('version', config.get('Config', 'eucalyptus-latest'))
+    product = request.values.get('product', 'eucalyptus')
+
+    mirrorlist = render_template('mirrorlist', mirrors=mirrors, release=release, arch=arch, distro=distro, version=version, product=product)
+
+    return Response(mirrorlist, mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
